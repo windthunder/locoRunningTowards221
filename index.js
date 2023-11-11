5d160091f5f8125e21790f5faaacccccaff741d2
nylon <WoW@MvM.com>
2023-11-11 20:12:27 +0800

    a

---
+++ index.js
@@ -0,0 +1,65 @@
+let run_num = 1;
+function running() {
+  run_num++;
+  if(run_num > 7){
+    run_num = 1;
+  }
+  $('.run').hide();
+  $('.run' + run_num).show();
+}
+
+
+$(() => {
+  let img_count = $('img').length;
+
+  let loaded = 0;
+  $('.run').one('load', () => {
+    loaded++;
+    $('#loading-progress').text(loaded / img_count * 220 + '%');
+    if (loaded == img_count) {
+      $('#main').css('visibility', 'visible');
+      $('#loading').hide();
+    }
+  }).each(function() {
+    if (this.complete) {
+      $(this).trigger('load');
+    }
+  });
+
+  let keybinds = {};
+
+  $('.button').each(function() {
+    let { keybind } = this.dataset;
+
+    if (keybind) {
+      keybinds[keybind.toLowerCase()] = this;
+    }
+  });
+
+  $(document).keyup((e) => {
+    let keybind = keybinds[e.key];
+
+    if (keybind) {
+      keybind.click();
+    }
+  })
+
+  let now = 'D';
+  $('.button').click(function() {
+    let { keybind } = this.dataset;
+
+    if((keybind == 'A' && now == 'D') || (keybind == 'D' && now == 'A')){
+      this.classList.add('active');
+      setTimeout(() => {
+        this.classList.remove('active');
+      }, 50);
+
+
+      running();
+      now = keybind;
+
+
+    }
+  })
+
+})
